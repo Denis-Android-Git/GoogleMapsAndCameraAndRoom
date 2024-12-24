4f6c436f0424cb09a42b6956e5698ef8ad992cc2
Denis-Android-Git <124072945+Denis-Android-Git@users.noreply.github.com>
2024-12-24 17:50:11 +0300

    n

--- app/src/main/java/com/example/myapplication/presentation/SearchScreen.kt
+++ app/src/main/java/com/example/myapplication/presentation/SearchScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
